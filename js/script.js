26bde2dd4ad9fb8c485eb6dd9931968777859708
mahibaz <102863275+mahibaz@users.noreply.github.com>
2023-08-09 11:33:29 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const filterButtons = document.querySelectorAll('.buttons');
+const filterImage = document.querySelectorAll('.image');
+
+const filterCards = e => {
+  document.querySelector('.active').classList.remove('active');
+  e.target.classList.add('active');
+  console.log(e.target)
+
+  filterImage.forEach(image => {
+    image.classList.add('hide');
+
+    if (image.dataset.name === e.target.dataset.name || e.target.dataset.name === 'all' ) {
+      image.classList.remove('hide');
+      
+    }
+  })
+}
+
+filterButtons.forEach( button => button.addEventListener('click',filterCards));
+
+filterImage.forEach(image =>{
+  image.onclick = () =>{
+    document.querySelector('.popup').style.display = 'block';
+    document.querySelector('.popup-image').src = image.getAttribute('src');
+}
+for (let index = 0; index < filterImage.length; index++) {
+  filterImage[index].setAttribute('onclick','preview(this)');
+  
+}
+  
+})
+
+
+
+const previewBox = document.querySelector('.popup');
+previewImg = previewBox.querySelector('img');
+closeIcon = previewBox.querySelector('.icon');
+
+function preview(element){
+  let selectedPrevImg = element.getAttribute('src');
+  previewImg.src = selectedPrevImg;
+  previewBox.classList.add('show');
+  closeIcon.onclick = () => {
+    previewBox.classList.remove('show');
+  } 
+}
